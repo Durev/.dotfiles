62142285caa30d9a3a8b6048440e88ba79a327ed
Vincent Durewski <vincent.durewski@gmail.com>
2022-08-19 11:11:43 +0200

    Zsh: Use LS_COLORS, update FZF settings

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -14,11 +14,16 @@ export PATH="$HOME/.local/bin":$PATH
 # Homebrew: Only update explicitely required packages
 export HOMEBREW_NO_AUTO_UPDATE=1
 
-# Setting fd as the default source for fzf (thus respecting .gitignore)
-export FZF_DEFAULT_COMMAND='ag -g ""'
+# fzf
+export FZF_DEFAULT_COMMAND="fd --type file --color=always --strip-cwd-prefix --hidden"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_CTRL_T_OPTS="--ansi --preview-window 'right:50%' --preview 'bat --color=always --style=header,grid,changes --line-range :300 {}'"
 
 # Use case-sensitive completion
 CASE_SENSITIVE="true"
 
 # Timestamp shown in the history command output - cf. 'man strftime'
 HIST_STAMPS="yyyy-mm-dd"
+
+# colorscheme for exa, lf, fd and tools using LS_COLORS
+export LS_COLORS="$(vivid generate nord)"
