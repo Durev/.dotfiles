a41c946d17b44c5884cf6b875b2ed65133e6fc50
Vincent Durewski <vincent.durewski@gmail.com>
2022-10-02 14:45:28 +0200

    Zsh: Change prompt colors

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -5,7 +5,7 @@ prompt spaceship
 # https://upload.wikimedia.org/wikipedia/commons/1/15/Xterm_256color_chart.svg
 SPACESHIP_GIT_STATUS_COLOR="172"
 SPACESHIP_CHAR_COLOR_SUCCESS="106"
-SPACESHIP_DIR_COLOR="069"
+SPACESHIP_DIR_COLOR="075"
 
 SPACESHIP_CHAR_SYMBOL="❯ "
 
